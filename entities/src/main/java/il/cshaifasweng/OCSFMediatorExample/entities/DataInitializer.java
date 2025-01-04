cc95a3d5e4090ea818b2fd86d39ecbb9238ba4fc
NoamBensh <benshaul2006@gmail.com>
2025-01-04 20:06:49 +0200

    database fixed3

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/DataInitializer.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/DataInitializer.java
@@ -4,12 +4,20 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 public class DataInitializer {
+
     public static void initializeData(Session session) {
         Transaction transaction = session.beginTransaction();
         try {
+            // רוקן את הטבלה
+            session.createQuery("delete from MenuItem").executeUpdate();
+
+            // אתחל את ה-AUTO_INCREMENT
+            session.createSQLQuery("ALTER TABLE MenuItems AUTO_INCREMENT = 1").executeUpdate();
+
+            // הוסף נתונים חדשים
             MenuItem item1 = new MenuItem("Salad", 35.00, "Tomatoes, cucumbers, lettuce", "Low calorie", null);
             MenuItem item2 = new MenuItem("Pizza", 55.00, "Mushrooms, onions, tomatoes", "Includes vegan option", null);
-            MenuItem item3 = new MenuItem("Pasta", 60.00, "Mushroom cream sauce", "Available gluten-free", null);
+            MenuItem item3 = new MenuItem("Pasta", 6000.00, "Mushroom cream sauce", "Available gluten-free", null);
             MenuItem item4 = new MenuItem("Hamburger", 80.00, "Meatball, pickle, tomato, lettuce", "Choice of meat or plant-based", null);
             MenuItem item5 = new MenuItem("Edamame", 30.00, "Edamame", "Served with sea salt", null);
 
@@ -28,4 +36,3 @@ public class DataInitializer {
         }
     }
 }
-
